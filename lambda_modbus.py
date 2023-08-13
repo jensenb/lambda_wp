49aadbc7e7fd0637b4492b5be2fe9e2845bbbb71
Brian Jensen <jensen.j.brian@gmail.com>
2023-08-13 10:39:30 +0200

    Add tags for operating state

--- lambda_modbus.py
+++ lambda_modbus.py
@@ -3,16 +3,15 @@ import asyncio
 import logging
 import logging.config
 import pathlib
-from typing import Dict, List, NoReturn
-from time import time
 from struct import pack, unpack
+from time import time
+from typing import Dict, List, NoReturn
 
 import influxdb_client
 from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
 from influxdb_client.client.write_api import SYNCHRONOUS, Point
 from pyModbusTCP.client import ModbusClient
 from ruamel.yaml import YAML
-
 
 HEAT_PUMP_MODE = {
     0: "STBY",
@@ -81,12 +80,7 @@ BUFFER_MODE = {
     9: "STBY-FROST",
 }
 
-SOLAR_MODE = {
-    0: "STBY",
-    1: "HEATING",
-    2: "ERROR",
-    3: "OFF"
-}
+SOLAR_MODE = {0: "STBY", 1: "HEATING", 2: "ERROR", 3: "OFF"}
 
 HEATING_CIRCUIT_STATE = {
     0: "HEATING",
@@ -128,8 +122,9 @@ HEAT_PUMP_REQUEST_TYPE = {
     1: "FLOW PUMP CIRCULATION",
     2: "CENTRAL HEATING",
     3: "CENTRAL COOLING",
-    4: "DOMESTIC HOT WATER"
+    4: "DOMESTIC HOT WATER",
 }
+
 
 def fixed_point_to_float_10(x):
     return float(x) / 10
@@ -180,15 +175,17 @@ def as_is(x):
 
 
 class ConnectionManager:
-    def __init__(self,
-                       lambda_host:str,
-                       lambda_port:int,
-                       influxdb_host: str,
-                       influxdb_port: int,
-                       influxdb_token: str,
-                       influxdb_org: str,
-                       influxdb_bucket: str):
-        
+    def __init__(
+        self,
+        lambda_host: str,
+        lambda_port: int,
+        influxdb_host: str,
+        influxdb_port: int,
+        influxdb_token: str,
+        influxdb_org: str,
+        influxdb_bucket: str,
+    ):
+
         self.lambda_host = lambda_host
         self.lambda_port = lambda_port
         self.bucket = influxdb_bucket
@@ -197,55 +194,52 @@ class ConnectionManager:
         self.influxdb_port = influxdb_port
         self.influxdb_token = influxdb_token
         self.influxdb_org = influxdb_org
-    
-    
+
     async def _init(self):
         self.lambda_client = ModbusClient(host=self.lambda_host, port=self.lambda_port)
 
-        self.influxdb_client = InfluxDBClientAsync(url=f"http://{self.influxdb_host}:{self.influxdb_port}",
-                                                   token=self.influxdb_token,
-                                                   org=self.influxdb_org
-                                                   )
+        self.influxdb_client = InfluxDBClientAsync(
+            url=f"http://{self.influxdb_host}:{self.influxdb_port}", token=self.influxdb_token, org=self.influxdb_org
+        )
         ready = await self.influxdb_client.ping()
         logging.debug(f"InfluxDB client ready: {ready}")
         self.write_api = self.influxdb_client.write_api()
 
-
     async def write_record(self, record: Point):
-        
+
         success = await self.write_api.write(bucket=self.bucket, record=record)
         if success:
             logging.debug(f"Successfully wrote {record} to influxdb")
         else:
             logging.error(f"Error writing record {record} to influxdb")
 
-
     def read_lambda_values(self, register, count):
         logging.debug(f"Reading register {register} from {self.lambda_host}:{self.lambda_port}")
         res = self.lambda_client.read_holding_registers(register, count)
         logging.debug(f"Retrieved {len(res)} values")
-        
+
         return res
 
-
-
-    async def read_lambda_vals(self,
+    async def read_lambda_vals(
+        self,
         register: int,
         measurement_group: str,
         names: List,
         funcs: List,
+        tags: List,
         count: int,
         format: str,
         interval=5.0,
-        cache_values: bool=True,
-        denoise_values: bool=True,
-        cache_timeout: float=300.0) -> NoReturn:
+        cache_values: bool = True,
+        denoise_values: bool = True,
+        cache_timeout: float = 300.0,
+    ) -> NoReturn:
 
         cached_vals = [[0.0, time()] for _ in names]
-    
+
         while True:
             res_enc = self.read_lambda_values(register=register, count=count)
-            
+
             # Modbus is big endian, pack the results in a byte string to be reinterpreted according
             # to the actual register type
             pack_format = ">" + "".join(["H" for _ in range(count)])
@@ -255,7 +249,7 @@ class ConnectionManager:
             p = Point(measurement_group)
             for i in range(len(names)):
                 logging.debug(f"Processing {names[i]}")
-                
+
                 # Skip values marked as None in the config
                 if names[i] == "None":
                     continue
@@ -263,51 +257,61 @@ class ConnectionManager:
                 val = funcs[i](vals[i])
                 t_now = time()
 
-                if cache_values:
+                # state info we write as tags on every measurement
+                if tags[i]:
+                    p = p.tag(names[i], val)
+                    logging.debug(f"Writing tag {names[i]}")
+
+                # caching only for field measurements
+                elif cache_values:
 
                     # We only denoise floating point values (from temp sensors primarily)
-                    if denoise_values and type(val) == float:
+                    if denoise_values:
                         if abs(cached_vals[i][0] - val) > 0.11 or t_now - cached_vals[i][1] >= cache_timeout:
-                            logging.info(f"Writing {names[i]}: {val}")
+                            logging.info(f"Writing field {names[i]}: {val}")
                             p = p.field(names[i], val)
                             cached_vals[i][0] = val
                             cached_vals[i][1] = t_now
                         else:
-                            logging.debug(f"Not writing {names[i]}")
+                            logging.debug(f"Not writing field {names[i]}")
 
                     elif cached_vals[i][0] != val or t_now - cached_vals[i][1] >= cache_timeout:
-                        logging.info(f"Writing {names[i]}: {val}")
+                        logging.info(f"Writing field {names[i]}: {val}")
                         p = p.field(names[i], val)
                         cached_vals[i][0] = val
                         cached_vals[i][1] = t_now
                     else:
-                        logging.debug(f"Not writing {names[i]}")
+                        logging.debug(f"Not writing field {names[i]}")
                 else:
-                    logging.info(f"Writing {names[i]}: {val}")
+                    logging.info(f"Writing field {names[i]}: {val}")
                     p = p.field(names[i], val)
-                
+
             await self.write_record(p)
             await asyncio.sleep(interval)
 
 
-async def create_connection_manager(lambda_host:str,
-                                    lambda_port:int,
-                                    influxdb_host: str,
-                                    influxdb_port: int,
-                                    influxdb_token: str,
-                                    influxdb_org: str,
-                                    influxdb_bucket: str) -> ConnectionManager:
+async def create_connection_manager(
+    lambda_host: str,
+    lambda_port: int,
+    influxdb_host: str,
+    influxdb_port: int,
+    influxdb_token: str,
+    influxdb_org: str,
+    influxdb_bucket: str,
+) -> ConnectionManager:
 
-    conn_man = ConnectionManager(lambda_host=lambda_host,
-                                 lambda_port=lambda_port,
-                                 influxdb_host=influxdb_host,
-                                 influxdb_port=influxdb_port,
-                                 influxdb_token=influxdb_token,
-                                 influxdb_org=influxdb_org,
-                                 influxdb_bucket=influxdb_bucket)
+    conn_man = ConnectionManager(
+        lambda_host=lambda_host,
+        lambda_port=lambda_port,
+        influxdb_host=influxdb_host,
+        influxdb_port=influxdb_port,
+        influxdb_token=influxdb_token,
+        influxdb_org=influxdb_org,
+        influxdb_bucket=influxdb_bucket,
+    )
 
     await conn_man._init()
-    
+
     return conn_man
 
 
@@ -317,28 +321,30 @@ async def main():
     """
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", help="Path to the configuration file", required=True)
-    parser.add_argument("--log-level",
+    parser.add_argument(
+        "--log-level",
         help="Log level used by the script",
         default="INFO",
-        choices=["DEBUG", "INFO", "WARNING", "ERROR"])
+        choices=["DEBUG", "INFO", "WARNING", "ERROR"],
+    )
     args = parser.parse_args()
 
     # setup logging
-    logging.basicConfig(level=args.log_level,
-                        format="%(asctime)s [%(levelname)s] %(module)s:%(lineno)d - %(message)s")
+    logging.basicConfig(level=args.log_level, format="%(asctime)s [%(levelname)s] %(module)s:%(lineno)d - %(message)s")
 
     # Load the specified config file
     yaml = YAML()
     config = yaml.load(pathlib.Path(args.config))
 
-    conn_man = await create_connection_manager(lambda_host=config["lambda"]["host"],
-                                               lambda_port=config["lambda"]["port"],
-                                               influxdb_host=config["influxdb"]["host"],
-                                               influxdb_port=config["influxdb"]["port"],
-                                               influxdb_token=config["influxdb"]["token"],
-                                               influxdb_org=config["influxdb"]["org"],
-                                               influxdb_bucket=config["influxdb"]["bucket"]
-                                               )
+    conn_man = await create_connection_manager(
+        lambda_host=config["lambda"]["host"],
+        lambda_port=config["lambda"]["port"],
+        influxdb_host=config["influxdb"]["host"],
+        influxdb_port=config["influxdb"]["port"],
+        influxdb_token=config["influxdb"]["token"],
+        influxdb_org=config["influxdb"]["org"],
+        influxdb_bucket=config["influxdb"]["bucket"],
+    )
 
     tasks = []
     for m_group in config["measurements"]:
@@ -348,6 +354,6 @@ async def main():
         tasks.append(t)
     await asyncio.gather(*tasks)
 
+
 if __name__ == "__main__":
     asyncio.run(main())
-
